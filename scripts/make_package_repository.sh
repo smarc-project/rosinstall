9052cb8f3a06db07253971f5456de38450704934
Nils Bore <nilsbore@gmail.com>
2020-12-26 12:45:38 +0100

    Create make_package_repository.sh

---
+++ scripts/make_package_repository.sh
@@ -0,0 +1,13 @@
+mkdir -p simple_debian_repo
+cd simple_debian_repo
+mkdir -p debian
+
+curl -s https://api.github.com/repos/nilsbore/smarc_msgs/releases/latest \
+| grep "bloom-release-deb.zip" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi -
+
+unzip bloom-release-deb.zip -d debian -o
+
+dpkg-scanpackages debian /dev/null | gzip -9c > debian/Packages.gz
