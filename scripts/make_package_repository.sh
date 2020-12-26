6e195504f31dbf90c74a968c8e580a143919e69f
Nils Bore <nilsbore@gmail.com>
2020-12-26 20:58:04 +0100

    Made it run with several repos

--- scripts/make_package_repository.sh
+++ scripts/make_package_repository.sh
@@ -1,12 +1,17 @@
 cd package_repo
 mkdir -p debian
 
-curl -s https://api.github.com/repos/nilsbore/smarc_msgs/releases/latest \
-| grep "bloom-release-deb.zip" \
-| cut -d : -f 2,3 \
-| tr -d \" \
-| wget -qi -
+while read line; do
+    pkg=$(echo $line | cut -c3-)
+    echo "Doing ${pkg}"
+    curl -s https://api.github.com/repos/${pkg}/releases/latest \
+    | grep "bloom-release-deb.zip" \
+    | cut -d : -f 2,3 \
+    | tr -d \" \
+    | wget -qi -
 
-unzip bloom-release-deb.zip -d debian #-o
+    unzip bloom-release-deb.zip -d debian/ #--out
+    rm bloom-release-deb.zip
+done < sources.yaml
 
 dpkg-scanpackages debian /dev/null | gzip -9c > debian/Packages.gz
